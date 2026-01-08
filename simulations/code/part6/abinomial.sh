1aa7427edea2dc84c41a3eb8f8f610d336b6fa14
Anonymousbot7 <hmnvrvgyp2@privaterelay.appleid.com>
2026-01-07 23:13:46 -0500

    Add files via upload

---
+++ simulations/code/part6/abinomial.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=part6bino
+#SBATCH --output=logdir/%j-%x.log
+#SBATCH --error=logdir/error_%j-%x.log
+#SBATCH --time=80:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=21
+#SBATCH --mem=50GB
+#SBATCH --partition=standard
+#SBATCH --account=youraccount
+#SBATCH --mail-type=ALL
+
+
+module --ignore_cache load "Python/3.12.1"
+
+
+export PATH=$HOME/.local/bin:$PATH
+export PYTHONPATH=$HOME/.local/lib/python3.12/site-packages:$PYTHONPATH
+
+
+python code/part6/abinomial.py --n $1 --index $2
+
